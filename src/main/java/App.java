2feae7ed676e8fcdced66ef98ec67ccb6f41b567
ESTOLEL <elijah.alixtair.estolas@oocl.com>
2022-08-26 15:03:22 +0800

    70% complete, refactored logic

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,5 +1,5 @@
 import java.util.*;
-import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.concurrent.atomic.AtomicReference;
 import java.util.stream.Collectors;
 
@@ -16,89 +16,51 @@ public class App {
     }
 
     public String bestCharge(List<String> inputs) {
-        App app = new App(new ItemService(), new SalesPromoService());
+        App app = startApp();
         String receipt = "";
         int promoIndex;
+        List<CartItem> finalCart = new ArrayList<>();
 
         AtomicReference<Double> cartTotal = new AtomicReference<>(0.0);
         Map cart = getCart(inputs);
         inputs = (List<String>) cart.keySet().stream().collect(Collectors.toList());
 
         final List<String> finalInputList = generateFinalInputList(inputs);
-        final Map finalCart = generateFinalCart(cart, finalInputList);
-        final Map promos = getApplicablePromos(finalInputList);
-        calculateCurrentCartTotal(cartTotal, finalCart);
-
-        if(promos.size()>0){
-            promoIndex = calculateOptimalPromo(cartTotal.get(),
-                    Arrays.asList(
-                        cartTotal.get()-6.0,
-                        calculateHalfPricePromo(finalCart, promos)
-                    )).get();
-        }else{
-            if(cartTotal.get()>=30){
-                promoIndex = 0;
-            }
-        }
-
-        receipt+= Constants.HEADER + "\n";
-        Map items = new HashMap();
-        finalCart.keySet().stream().forEach(cartItem -> {
-            String itemName = itemService.findAll().stream().filter(item -> {
-                return cartItem.equals(item.getId());
-            }).findFirst().get().getName();
-            items.put(itemName, finalCart.get(cartItem));
-        });
-
-        System.out.println(cartTotal);
+        generateFinalCart(finalCart, cart, finalInputList);
 
         return receipt;
     }
 
-    private AtomicInteger calculateOptimalPromo(Double currentTotal, List<Double> discountedTotals){
-        Double saved = currentTotal;
-        AtomicInteger promoId = new AtomicInteger();
-        discountedTotals.forEach(discountedTotal -> {
-            if(currentTotal-discountedTotal < saved) promoId.set(discountedTotals.indexOf(discountedTotal));
-        });
-        return promoId;
-    }
-
-    private Double calculateHalfPricePromo( Map finalCart, Map promos) {
-
-        AtomicReference<Double> newCartTotal = new AtomicReference<>(0.0);
-        finalCart.keySet().stream().forEach(cartItem ->{
-            itemService.findAll().stream().forEach(item -> {
-                if(item.getId().equals(cartItem)) newCartTotal.updateAndGet(value ->
-                        promos.keySet().contains(item.getId())?
-                        value + (item.getPrice() * Double.parseDouble((String) finalCart.get(item.getId()))/2) :
-                        value + (item.getPrice() * Double.parseDouble((String) finalCart.get(item.getId())))
-                );
-            });
-        });
-        return newCartTotal.get();
-    }
-    private void calculateCurrentCartTotal(AtomicReference<Double> cartTotal, Map finalCart) {
-        finalCart.keySet().stream().forEach(cartItem ->{
-            itemService.findAll().stream().forEach(item -> {
-                if(item.getId().equals(cartItem)) cartTotal.updateAndGet(value ->
-                        value + (item.getPrice() * Double.parseDouble((String) finalCart.get(item.getId())))
-                );
-            });
-        });
-    }
-
-    private Map getApplicablePromos(List<String> finalInputList) {
-        Map promos = new HashMap();
+    private void generateFinalCart(List<CartItem> finalCart, Map cart, List<String> finalInputList) {
+        App app;
+        try{
         finalInputList.forEach(input -> {
-            salesPromoService.findAll().stream().forEach(promo -> {
-                if(promo.getRelatedItems().contains(input)){
-                    promos.put(input,promo);
-                }
-            });
+            List<SalesPromotion> possiblePromos = new ArrayList<>();
+            Item matchedItem = itemService.findAll().stream().filter(item -> {
+                return item.getId().equals(input);
+            }).findFirst().get();
+
+            Double price = matchedItem.getPrice();
+            String name = matchedItem.getName();
+            String id = matchedItem.getId();
+            int amount = Integer.parseInt(cart.get(input).toString());
+            possiblePromos = salesPromoService.findAll().stream().filter(salesPromotion -> {
+              return salesPromotion.getRelatedItems().contains(input);
+            }).collect(Collectors.toList());
+            finalCart.add(new CartItem(id, name, price, possiblePromos,amount));
+            System.out.println(possiblePromos);
         });
-        return promos;
+        }catch(NumberFormatException e){
+            System.out.println("INVALID INPUT!\nPlease try again");
+            app = startApp();
+        }
     }
+
+    private static App startApp() {
+        App app = new App(new ItemService(), new SalesPromoService());
+        return app;
+    }
+
 
     private static Map getCart(List<String> inputs) {
         Map cart = new HashMap();
@@ -108,15 +70,6 @@ public class App {
         });
         return cart;
     }
-
-    private Map generateFinalCart(Map cart, List<String> finalInputList) {
-        Map finalCart = new HashMap();
-        finalInputList.forEach(input -> {
-            finalCart.put(input, cart.get(input));
-        });
-        return finalCart;
-    }
-
     private List<String> generateFinalInputList(List<String> inputs) {
         return inputs.stream().filter(input -> {
             return itemService.findAll().stream().anyMatch(item -> {
--- src/main/java/CartItem.java
+++ src/main/java/CartItem.java
@@ -1,7 +1,19 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class CartItem extends Item{
-    String promo;
-    public CartItem(String id, String name, double price, String promo) {
-        super(id, name, price);
-        this.promo = promo;
+    public void setPossiblePromos(List<SalesPromotion> possiblePromos) {
+        this.possiblePromos = possiblePromos;
     }
+
+    List<SalesPromotion> possiblePromos = new ArrayList<>();
+    int amount;
+
+    public CartItem(String id, String name, double price, List<SalesPromotion> possiblePromos, int amount) {
+        super(id, name, price);
+        this.possiblePromos = possiblePromos;
+        this.amount = amount;
+    }
+
+
 }
